6d83e582fd83229dbcd778fafa27e6f051116a42
Jan Szyprzak <jan.szyprzak@gmail.com>
2023-05-27 13:17:57 +0200

    Fix validation of form data

--- dict/views.py
+++ dict/views.py
@@ -14,15 +14,21 @@ def get_completion(prompt, model="gpt-3.5-turbo"):
     )
     return response.choices[0].message["content"]
 
+
+
 def dict_view(request):
     if request.method == "POST":
         form = DictForm(request.POST)
         if form.is_valid():
-            prompt = form.cleaned_data['prompt']
+            text = form.cleaned_data['text']
             language = form.cleaned_data['language']
-            # Perform the translation with the get_completion function
+
+
+            prompt = f"Translate text delimited by <> brackets into {language} language. <{text}>"
+
             completion = get_completion(prompt)
-            return render(request, 'dict.html', {'form': form, 'completion': completion})
+
+            return render(request, 'dict/dict.html', {'form': form, 'completion': completion})
     else:
         form = DictForm()
-    return render(request, 'dict.html', {'form': form})
+    return render(request, 'dict/dict.html', {'form': form})
