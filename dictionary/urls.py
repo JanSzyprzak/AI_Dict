ad54c7e929b2fa8b9306d772bce3e72b929f77cb
Jan Szyprzak <jan.szyprzak@gmail.com>
2023-05-27 13:18:57 +0200

    Update

--- dictionary/urls.py
+++ dictionary/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from dict.views import dict_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', dict_view, name='dict'),
 ]
